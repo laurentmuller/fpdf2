5f6829b32b0ee59c122169c548e267fe3294c19a
Andreas Müller <mail@devmount.de>
2025-01-10 10:35:19 +0100

    ➕ Add tests for PDF attachment trait

---
+++ tests/Traits/PdfAttachmentTraitTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the 'fpdf' package.
+ *
+ * For the license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author bibi.nu <bibi@bibi.nu>
+ */
+
+declare(strict_types=1);
+
+namespace fpdf\Tests\Traits;
+
+use fpdf\Enums\PdfDestination;
+use fpdf\PdfDocument;
+use fpdf\Traits\PdfAttachmentTrait;
+use PHPUnit\Framework\TestCase;
+
+class PdfAttachmentTraitTest extends TestCase
+{
+    public function testNoAttachments(): void
+    {
+        $document = new class() extends PdfDocument {
+            use PdfAttachmentTrait;
+        };
+        $document->addPage();
+        $document->output(PdfDestination::STRING);
+        self::assertSame(1, $document->getPage());
+    }
+
+    public function testAttachment(): void
+    {
+        $document = new class() extends PdfDocument {
+            use PdfAttachmentTrait;
+        };
+        $document->addPage();
+        $document->attach('tests/resources/attachment.txt');
+        $document->openAttachmentPane();
+        $document->output(PdfDestination::STRING);
+        self::assertSame(1, $document->getPage());
+    }
+}
